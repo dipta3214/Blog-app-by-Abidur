1ba453412e8437ca4d47f5bad903ee2ac6df2b5d
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2021-12-17 12:03:44 -0500

    updated update image issue

--- blog/src/components/ArticleDetails.jsx
+++ blog/src/components/ArticleDetails.jsx
@@ -50,7 +50,7 @@ const ArticleDetails = (props) => {
             <br /><br /><br /><br />
             <form onSubmit={(e) => submitForm(e)}>
                 <input type="text" value={newPost.title} id="title" placeholder="Enter Your title" onChange={(e) => handleChange(e)} required />
-                <input type="url" value={newPost.image} id="image" placeholder="Enter Image URL" onChange={(e) => handleChange(e)} />
+                <input type="text" value={newPost.image} id="image" placeholder="Enter Image URL" onChange={(e) => handleChange(e)} />
                 <input type="textarea" value={newPost.content} id="content" placeholder="Enter Your Content" onChange={(e) => handleChange(e)} required />
                 <input type="text" value={newPost.category} id="category" placeholder="Enter Category Name" onChange={(e) => handleChange(e)} required />
                 <button type="submit">Submit</button>
