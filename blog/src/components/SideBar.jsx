859d174032e1ac5739c3b19dbd69138104544bf1
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2021-12-20 15:36:40 -0500

    added a recent jsx

--- blog/src/components/SideBar.jsx
+++ blog/src/components/SideBar.jsx
@@ -6,6 +6,8 @@ const SideBar = () => {
         <div className="SideBar">
             <div className="sidebar">
                 <h4 style={{ color: 'white' }}>Categories:</h4>
+                <Link to='/' style={{ color: 'white', textDecoration: 'none', padding: '2vh' }}>All</Link>
+                <Link to='/recent' style={{ color: 'white', textDecoration: 'none', padding: '2vh' }}>Recent</Link>
                 <Link to='/entertainment' style={{ color: 'white', textDecoration: 'none', padding: '2vh' }}>Entertainment</Link>
                 <Link to='/sports' style={{ color: 'white', textDecoration: 'none', padding: '2vh' }}>Sports</Link>
                 <Link to='/tech' style={{ color: 'white', textDecoration: 'none', padding: '2vh' }}>Tech</Link>
