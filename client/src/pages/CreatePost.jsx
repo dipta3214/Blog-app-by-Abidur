24949c66a2c45be56fe42bd5233be26f31ddb067
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2022-04-06 18:13:46 -0400

    changing form style

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,79 +1,128 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import SuccesfulCreation from "./SuccessfulCreation";
-import Modal from "../components/Modal";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SuccesfulCreation from './SuccessfulCreation';
+import Modal from '../components/Modal';
 
 const CreatePost = (props) => {
+  const [user, setUser] = useState([]);
 
-    const [user, setUser] = useState([])
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get('/api/users');
+      setUser(res.data.users[0]);
+    };
+    getUser();
+  }, []);
 
-    useEffect(() => {
-        const getUser = async () => {
-            const res = await axios.get('/api/users')
-            setUser(res.data.users[0])
-        }
-        getUser()
-    }, [])
+  const [newPost, setNewPost] = useState({
+    title: ``,
+    content: ``,
+    image: ``,
+    category: ``,
+    author_name: user.author
+  });
 
-    const [newPost, setNewPost] = useState({
+  const handleChange = (e) => {
+    const newestPost = { ...newPost };
+    newestPost[e.target.id] = e.target.value;
+    setNewPost(newestPost);
+    console.log(newPost);
+  };
+
+  const [message, setMessage] = useState(false);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    axios.post('/api/createArticle', {
+      title: newPost.title,
+      content: newPost.content,
+      image: newPost.image,
+      category: newPost.category,
+      author_name: user.author
+    });
+
+    if (
+      newPost.title !== '' &&
+      newPost.content !== '' &&
+      newPost.image !== '' &&
+      newPost.category !== ''
+    ) {
+      setMessage(true);
+      let afterSubmit = {
         title: ``,
         content: ``,
         image: ``,
         category: ``,
         author_name: user.author
-    })
-
-    const handleChange = (e) => {
-        const newestPost = { ...newPost }
-        newestPost[e.target.id] = e.target.value
-        setNewPost(newestPost)
-        console.log(newPost)
+      };
+      setNewPost(afterSubmit);
     }
+  };
 
-    const [message, setMessage] = useState(false)
+  return (
+    <div className="form-div-div">
+      <SuccesfulCreation open={message} />
+      <div className="form-div h-100">
+        <h2>New Post:</h2>
+        <form onSubmit={(e) => submitForm(e)} className="form">
+          <div className="form-child">
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              value={newPost.title}
+              id="title"
+              className="inputForm"
+              placeholder="Enter Your title"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
 
-    const submitForm = (e) => {
-        e.preventDefault()
-        axios.post('/api/createArticle', {
-            title: newPost.title,
-            content: newPost.content,
-            image: newPost.image,
-            category: newPost.category,
-            author_name: user.author
-        })
+          <div className="form-child">
+            <label htmlFor="image">Image(URL Only):</label>
+            <input
+              type="text"
+              value={newPost.image}
+              id="image"
+              className="inputForm"
+              placeholder="Enter Image URL"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+          <div className="form-child">
+            <label htmlFor="content">Content:</label>
+            <textarea
+              type="text"
+              value={newPost.content}
+              id="content"
+              className="inputForm"
+              placeholder="Enter Your Content"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+          <div className="form-child">
+            <label htmlFor="category">Category:</label>
+            <input
+              type="text"
+              value={newPost.category}
+              id="category"
+              className="inputForm"
+              placeholder="Enter Category Name"
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+          <div>
+            <button className="form-btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-        if (newPost.title !== '' && newPost.content !== '' && newPost.image !== '' && newPost.category !== '') {
-            setMessage(true)
-            let afterSubmit = {
-                title: ``,
-                content: ``,
-                image: ``,
-                category: ``,
-                author_name: user.author
-            }
-            setNewPost(afterSubmit)
-        }
-    }
-
-    return (
-        <div className="form-div-div">
-            <SuccesfulCreation open={message} />
-            <div className="form-div">
-                <form onSubmit={(e) => submitForm(e)} className="form">
-                    <h2>New Post:</h2>
-                    <label for='title'>Title:</label>
-                    <input type="text" value={newPost.title} id="title" placeholder="Enter Your title" onChange={(e) => handleChange(e)} required />
-                    <label for='image'>Image(URL Only):</label>
-                    <input type="text" value={newPost.image} id="image" placeholder="Enter Image URL" onChange={(e) => handleChange(e)} required />
-                    <label for='content'>Content:</label>
-                    <textarea type="text" value={newPost.content} id="content" placeholder="Enter Your Content" onChange={(e) => handleChange(e)} required />
-                    <label for='category'>Category:</label>
-                    <input type="text" value={newPost.category} id="category" placeholder="Enter Category Name" onChange={(e) => handleChange(e)} required />
-                    <button className="form-btn" type="submit">Submit</button>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default CreatePost
+export default CreatePost;
